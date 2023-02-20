64156519792f1b647cb14aaedd42f811ebba5286
ThalitaCesar <thalita.walleska@gmail.com>
2023-02-20 15:49:35 -0300

    fix

--- Boilerplate/.next/static/chunks/webpack.js
+++ Boilerplate/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c35879db4dd56b69"; }
+/******/ 		__webpack_require__.h = function() { return "6b812e7636a74cca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
